5f21d23f962bbf6084b09b240b0becc0b8835bf9
Egor <egorshah@gmail.com>
2015-06-04 00:38:44 +0300

    polling

--- bipiper/polling.c
+++ bipiper/polling.c
@@ -20,70 +20,6 @@
 
 #define PRINT_DEBUG(_X) printf("Var %s = %d\n", #_X, _X)
 
-int resend_data(int fromfd, int tofd)
-{
-    int exit_status = EXIT_SUCCESS;
-    struct buf_t* buf = buf_new(BUF_SIZE);
-    if (buf == NULL)
-    {
-        exit_status = -1;
-        goto close_sockets;
-    }
-    ssize_t readed = -1;
-    for (;;)
-    {
-        readed = buf_fill(fromfd, buf, 1);
-        if (readed == -1)
-        {
-            exit_status = EXIT_FAILURE;
-            break;
-        }
-        if (readed == 0)
-        {
-            break;
-        }
-        ssize_t written = buf_flush(tofd, buf, 1);
-        if (written == -1)
-        {
-            exit_status = EXIT_FAILURE;
-            break;
-        }
-    }
-    buf_free(buf);
-close_sockets:
-    close(fromfd);
-    close(tofd);
-    return exit_status;
-}
-
-int bipipe(int cfd1, int cfd2)
-{
-    int result = 0;
-    pid_t first_pid = 0;
-    switch (first_pid = fork())
-    {
-        case -1:
-            result = -1;
-            goto bipipe_end;
-        case 0:
-            exit(resend_data(cfd1, cfd2));
-            break;
-    }
-    switch (fork())
-    {
-        case -1:
-            kill(first_pid, SIGKILL);
-            result = -1;
-            goto bipipe_end;
-        case 0:
-            exit(resend_data(cfd2, cfd1));
-    }
-bipipe_end:
-    close(cfd1);
-    close(cfd2);
-    return result;
-}
-
 int listen_port(char* port)
 {
     struct addrinfo hints;
@@ -93,12 +29,8 @@ int listen_port(char* port)
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
     hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
-    hints.ai_protocol = 0;          /* Any protocol */
-    hints.ai_canonname = NULL;
-    hints.ai_addr = NULL;
-    hints.ai_next = NULL;
 
-    s = getaddrinfo(NULL, port, &hints, &result);
+    s = getaddrinfo("localhost", port, &hints, &result);
     RET_IF(s != 0)
 
    /* getaddrinfo() returns a list of address structures.
@@ -113,6 +45,8 @@ int listen_port(char* port)
         {
             continue;
         }
+        int yes = 1;
+        setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
         if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0)
         {
@@ -142,6 +76,13 @@ int get_client(int sfd)
     return cfd;
 }
 
+int make_non_blocking(int fd)
+{
+    int opts = fcntl(fd, F_GETFL, 0);
+    RET_IF(fcntl(fd, F_SETFL, opts | O_NONBLOCK) < 0);
+    return 0;
+}
+
 void sig_handler_forking(int signo)
 {
     if (signo == SIGCHLD)
@@ -161,31 +102,48 @@ void set_sig_handler()
 }
 
 typedef struct {
-    char* buf1;
-    char* buf2;
+    struct buf_t* buf[2];
 } pair_buffers_t;
 
 struct pollfd fds[256];
 pair_buffers_t buffs[127];
-int client_size;
+int fds_size;
 
+void add_clients(int cfd1, int cfd2)
+{
+    buffs[fds_size/2 - 1].buf[0] = buf_new(BUF_SIZE);
+    buffs[fds_size/2 - 1].buf[1] = buf_new(BUF_SIZE);
+    fds[fds_size].fd = cfd1;
+    fds[fds_size].events = POLLIN | POLLHUP;
+    ++fds_size;
+    fds[fds_size].fd = cfd2;
+    fds[fds_size].events = POLLIN | POLLHUP;
+    ++fds_size;
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 3)
     {
-        dprintf(STDERR_FILENO, "Usage: port filename\n");
+        dprintf(STDERR_FILENO, "Usage: port1 por2\n");
         exit(EXIT_FAILURE);
     }
     set_sig_handler();
     char* const port1 = argv[1];
     char *const port2 = argv[2];
-    int sfd1 = listen_port(port1);
-    int sfd2 = listen_port(port2);
-    EXIT_IF(sfd1 == -1)
-    EXIT_IF(sfd2 == -1)
+    fds[0].fd = listen_port(port1);
+    EXIT_IF(fds[0].fd == -1);
+    fds[0].events = POLLIN;
+    make_non_blocking(fds[0].fd);
+    fds[1].fd = listen_port(port2);
+    EXIT_IF(fds[1].fd == -1);
+    fds[1].events = POLLIN;
+    make_non_blocking(fds[1].fd);
+    fds_size = 2;
 
+    int state = 0;
+    int cfd1 = -1;
     for (;;) {
-        int count = poll(fds, client_size, 1000);
+        int count = poll(fds, fds_size, 2000);
         if (count == 0)
         {
             continue;
@@ -194,20 +152,68 @@ int main(int argc, char* argv[]) {
         {
             break;
         }
-        int cfd1 = get_client(sfd1);
-        if (cfd1 == 0) continue;
-        int cfd2 = get_client(sfd2);
-        while (cfd2 == 0)
+        if (fds[state].revents & POLLIN)
         {
-            cfd2 = get_client(sfd2);
+            int client_fd = get_client(fds[state].fd);
+            if (state == 0)
+            {
+                cfd1 = client_fd;
+            }
+            else
+            {
+                add_clients(cfd1, client_fd);
+            }
+            state = state ^ 1;
         }
-        if (cfd1 == -1 || cfd2 == -1)
+        for (int i = 2; i < fds_size; ++i)
         {
-            close(sfd1);
-            close(sfd2);
-            exit(EXIT_FAILURE);
+            if ((fds[i].revents & POLLHUP)
+                    || (fds[i].revents & POLLERR))
+            {
+closing_client_pair:
+                close(fds[i].fd);
+                close(fds[i^1].fd);
+                fds[i].fd = -fds[i].fd;
+                fds[i^1].fd = -fds[i^1].fd;
+                if ((i&1) == 0)
+                {
+                    ++i;
+                }
+                continue;
+            }
+            if (fds[i].revents & POLLIN)
+            {
+                struct buf_t* buf = buffs[i/2 - 1].buf[i&1];
+                int old_size = buf_size(buf);
+                if (buf_fill(fds[i].fd, buf, old_size + 1) <= old_size) { goto closing_client_pair; }
+                if (buf_size(buf) == buf_capacity(buf))
+                {
+                    fds[i].events ^= POLLIN;
+                }
+                if (buf_size(buf) > 0)
+                {
+                    fds[i^1].events |= POLLOUT;
+                }
+            }
+            if (fds[i].revents & POLLOUT)
+            {
+                struct buf_t* buf = buffs[i/2 - 1].buf[(i&1)^1];
+                if (buf_flush(fds[i].fd, buf, 1) == -1) { goto closing_client_pair; }
+                if (buf_size(buf) == 0)
+                {
+                    fds[i].events ^= POLLOUT;
+                }
+            }
         }
-        bipipe(cfd1, cfd2);
+        for (int i = 2; i < fds_size; i+=2)
+        {
+            if (fds[i].fd < 0)
+            {
+                fds[i] = fds[fds_size - 2];
+                fds[i+1] = fds[fds_size - 1];
+                fds_size -= 2;
+            }
+        }
     }
 }
 
